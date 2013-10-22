d25c5a2bb18540b08b929ef0c0f22f3968d51397
michaelsharpe <m.sharpe@project89.org>
2013-10-22 11:07:46 -0400

    add exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,11 @@
+#question 1
+puts "A fair tip for a $55.00 meal is $#{55.0 * 0.15}."
+
+#question 2
+puts "I am adding " + 3.to_s + " to this string."
+
+#question 3
+puts 45628 * 7839
+
+#question 4
+puts (true && false) || (false && true) || !(false && false)
