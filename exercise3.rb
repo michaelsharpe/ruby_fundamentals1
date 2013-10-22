b7a047bf039e6eb0c4905a16b42ae3d952d65a17
michaelsharpe <m.sharpe@project89.org>
2013-10-22 11:22:57 -0400

    update exercise 3

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,10 @@
 puts "What is your name?"
 name = gets.chomp
-puts "Hi, #{name}!"
+
+puts "How old are you?"
+age = gets.chomp.to_i
+
+puts "What year were you born?"
+year = gets.chomp.to_i
+
+puts "Hi, #{name}! You are #{age} and were born in #{year}"
